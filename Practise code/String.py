e44b1c17950f1f0e8c25ee265cc39b724cf759d3
QA <pushp.bardhan@gmail.com>
2022-03-18 13:01:10 +0530

    String practice

--- Practise code/String.py
+++ Practise code/String.py
@@ -1,1 +1,23 @@
-a = "My name is Pushpender"
+# a = input("Enter the Name: \n")
+# b = "Good afternoon, "
+# print (b+ a)
+
+# letter = '''Dear <|Name|>
+# You are selected
+#  <|Date|>'''
+# name = input("Enter Name \n")
+# date = input("Enter date \n")
+# letter = letter.replace("<|Name|>", name)
+# letter = letter.replace("<|Date|>", date)
+# print(letter)
+
+# letter = "My Love my  country"
+# double = letter.find("  ")
+# print(double)
+# letter = letter.replace("  ", " ")
+# print(letter)
+# double = letter.find("  ")
+# print(double)
+
+letter = "Dear Harry, \n    This python course is nice \nThanks!"
+print(letter)
